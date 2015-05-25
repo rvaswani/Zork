70aec27f5cbb74329e1300a97bca598a2e1a1ee8
rvaswani <rvaswani@bayviewglen.ca>
2015-05-25 15:34:26 -0400

    Initial Commit

---
+++ Zork Game/src/com/bayviewglen/ZorkGame/Room2.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.ZorkGame;
+
+public class Room2 {
+
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
---
+++ Zork Game/src/com/bayviewglen/ZorkGame/Zork.java
@@ -0,0 +1,10 @@
+package com.bayviewglen.ZorkGame;
+
+public class Zork {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+
+}
